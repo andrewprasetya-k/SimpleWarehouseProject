b4507e2bdff04a7468fb137f2aa63f1099dbe506
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-08-31 14:38:50 +0700

    feat: added GlobalExceptionHandler

---
+++ src/main/java/org/warehouse/Controller/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package org.warehouse.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", message));
+    }
+}
