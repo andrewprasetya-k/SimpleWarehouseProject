23f3e0bc4ca510cfd1a05df2635083107820bbf8
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-08-31 15:55:36 +0700

    feat: added physical item response and request dto
    Signed-off-by: andrewprasetya-k <andrewprasetya.k@gmail.com>

---
+++ src/main/java/org/warehouse/Dto/PhysicalItemRequest.java
@@ -0,0 +1,50 @@
+package org.warehouse.Dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public class PhysicalItemRequest {
+    @NotNull(message = "Item name cannot be empty")
+    private String itemName;
+
+    @Positive(message = "Quantity name must be > 0")
+    private Integer quantity;
+
+    @Positive(message = "Price must be > 0")
+    private Double price;
+
+    @Positive(message = "Weight must be > 0")
+    private double weight;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
