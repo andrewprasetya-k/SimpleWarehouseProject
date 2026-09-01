dc862ac92bc65e4191007140dc9f6e133d15d173
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-09-01 10:26:02 +0700

    refactor: refactor response DTO class into record
    Signed-off-by: andrewprasetya-k <andrewprasetya.k@gmail.com>

--- src/main/java/org/warehouse/Dto/PhysicalItemResponse.java
+++ src/main/java/org/warehouse/Dto/PhysicalItemResponse.java
@@ -1,38 +1,4 @@
 package org.warehouse.Dto;
 
-public class PhysicalItemResponse {
-    private Integer id;
-    private String itemName;
-    private Integer quantity;
-    private Double price;
-    private double weight;
-
-    public PhysicalItemResponse(Integer id, String itemName, Integer quantity, Double price, double weight) {
-        this.id = id;
-        this.itemName = itemName;
-        this.quantity = quantity;
-        this.price = price;
-        this.weight = weight;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public String getItemName() {
-        return itemName;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
+public record PhysicalItemResponse(Integer id, String itemName, Integer quantity, Double price, double weight) {
 }
