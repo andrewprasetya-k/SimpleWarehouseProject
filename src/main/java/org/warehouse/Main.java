c03116cdcff2f93fa6f4a8ed5ed911bb38930a1d
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-09-03 08:58:02 +0700

    feat: edited main to print beans
    Signed-off-by: andrewprasetya-k <andrewprasetya.k@gmail.com>

--- src/main/java/org/warehouse/Main.java
+++ src/main/java/org/warehouse/Main.java
@@ -1,10 +1,14 @@
 package org.warehouse;
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        ApplicationContext ctx = SpringApplication.run(Main.class, args);
+//        for(String beanName : ctx.getBeanDefinitionNames()) {
+//            System.out.println(ctx.getBean(beanName));
+//        }
     }
 }
