26ee8487225114a9fb65ea3ae4b2f9873b0df55d
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-09-03 08:58:18 +0700

    chore: change event listener to bahasa
    Signed-off-by: andrewprasetya-k <andrewprasetya.k@gmail.com>

--- src/main/java/org/warehouse/Event/ItemsMovedListener.java
+++ src/main/java/org/warehouse/Event/ItemsMovedListener.java
@@ -8,17 +8,17 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class ItemsMovedListener {
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void onBeforeCommit(ItemsMovedEvent event) {
-        System.out.println("Mau commit... item " + event.getItemsId() + " ke warehouse " + event.getWarehouseId());
+        System.out.println("Committing... item " + event.getItemsId() + " to warehouse " + event.getWarehouseId());
     }
 
     @TransactionalEventListener(phase= TransactionPhase.AFTER_COMMIT)
     public void itemsMovedEvent(ItemsMovedEvent event) {
-        System.out.println("Sukses permanen! Item " + event.getItemsId() + " pindah ke warehouse " + event.getWarehouseId());
+        System.out.println("Success! Item " + event.getItemsId() + " is moved to warehouse " + event.getWarehouseId());
         //tempat untuk tambahkan push notification/email, dll
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
     public void onAfterRollback(ItemsMovedEvent event) {
-        System.out.println("Gagal, membatalkan memindahkan item "+ event.getItemsId()+" pindah ke warehouse "+event.getWarehouseId());
+        System.out.println("Failed to moved "+ event.getItemsId()+" to warehouse "+event.getWarehouseId());
     }
 }
