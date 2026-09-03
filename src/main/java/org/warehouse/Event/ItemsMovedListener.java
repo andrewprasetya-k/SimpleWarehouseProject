5186782ed9c843f6e9ed562eb35bc3ef9545bd3b
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-09-03 07:32:51 +0700

    feat: added before commit
    Signed-off-by: andrewprasetya-k <andrewprasetya.k@gmail.com>

--- src/main/java/org/warehouse/Event/ItemsMovedListener.java
+++ src/main/java/org/warehouse/Event/ItemsMovedListener.java
@@ -6,6 +6,11 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class ItemsMovedListener {
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    public void onBeforeCommit(ItemsMovedEvent event) {
+        System.out.println("Mau commit... item " + event.getItemsId() + " ke warehouse " + event.getWarehouseId());
+    }
+
     @TransactionalEventListener(phase= TransactionPhase.AFTER_COMMIT)
     public void itemsMovedEvent(ItemsMovedEvent event) {
         System.out.println("Sukses permanen! Item " + event.getItemsId() + " pindah ke warehouse " + event.getWarehouseId());
