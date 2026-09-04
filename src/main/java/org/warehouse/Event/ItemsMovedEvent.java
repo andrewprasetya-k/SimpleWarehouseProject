0e1160b3a6b4e41e22934996f521d81a4dbe5ad1
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-09-04 13:01:29 +0700

    refactor: change ItemsMovedEvent.java to record
    Signed-off-by: andrewprasetya-k <andrewprasetya.k@gmail.com>

--- src/main/java/org/warehouse/Event/ItemsMovedEvent.java
+++ src/main/java/org/warehouse/Event/ItemsMovedEvent.java
@@ -2,20 +2,5 @@ package org.warehouse.Event;
 
 import java.util.List;
 
-public class ItemsMovedEvent {
-    public final Integer warehouseId;
-    public final List<Integer> itemsId;
-
-    public ItemsMovedEvent(Integer warehouseId, List<Integer> itemsId) {
-        this.warehouseId = warehouseId;
-        this.itemsId = itemsId;
-    }
-
-    public Integer getWarehouseId() {
-        return warehouseId;
-    }
-
-    public List<Integer> getItemsId() {
-        return itemsId;
-    }
+public record ItemsMovedEvent(Integer warehouseId, List<Integer> itemsId) {
 }
--- src/main/java/org/warehouse/Event/ItemsMovedListener.java
+++ src/main/java/org/warehouse/Event/ItemsMovedListener.java
@@ -8,17 +8,17 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class ItemsMovedListener {
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void onBeforeCommit(ItemsMovedEvent event) {
-        System.out.println("Committing... item " + event.getItemsId() + " to warehouse " + event.getWarehouseId());
+        System.out.println("Committing... item " + event.itemsId() + " to warehouse " + event.warehouseId());
     }
 
     @TransactionalEventListener(phase= TransactionPhase.AFTER_COMMIT)
     public void itemsMovedEvent(ItemsMovedEvent event) {
-        System.out.println("Success! Item " + event.getItemsId() + " is moved to warehouse " + event.getWarehouseId());
+        System.out.println("Success! Item " + event.itemsId() + " is moved to warehouse " + event.warehouseId());
         //tempat untuk tambahkan push notification/email, dll
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
     public void onAfterRollback(ItemsMovedEvent event) {
-        System.out.println("Failed to moved "+ event.getItemsId()+" to warehouse "+event.getWarehouseId());
+        System.out.println("Failed to moved "+ event.itemsId()+" to warehouse "+event.warehouseId());
     }
 }
