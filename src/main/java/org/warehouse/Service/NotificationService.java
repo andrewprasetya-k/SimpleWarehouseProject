74e492c7040d8b082eed52a1bef68a20a7be58eb
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-09-14 14:04:48 +0700

    refactor: refactor NotificationService.java to webflux
    Signed-off-by: andrewprasetya-k <andrewprasetya.k@gmail.com>

--- src/main/java/org/warehouse/Service/NotificationService.java
+++ src/main/java/org/warehouse/Service/NotificationService.java
@@ -1,12 +1,31 @@
 package org.warehouse.Service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.warehouse.Kafka.Dto.ItemsMovedKafkaMessage;
+import reactor.core.publisher.Mono;
 
 @Service
 public class NotificationService {
+    private final WebClient webClient;
+
+    public NotificationService(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     public void notifyItemsMoved(ItemsMovedKafkaMessage message) {
-        System.out.println("NOTIFICATION_ITEMS_MOVED itemId " + message.itemIds() + " has been moved to warehouse "+message.warehouseId());
+        System.out.println(
+                "NOTIFICATION_ITEMS_MOVED warehouseId=" + message.warehouseId()
+                        + " itemIds=" + message.itemIds()
+                        + " status=" + message.status()
+        );
+    }
+
+    public Mono<Boolean> checkHealth() {
+        return webClient.get()
+                .uri("http://localhost:8080/health")
+                .retrieve()
+                .toBodilessEntity()
+                .map(response -> response.getStatusCode().is2xxSuccessful());
     }
 }
