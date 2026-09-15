b20b7253f3bd61ef7df85f3d70fd231db75312b9
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-09-15 13:54:37 +0700

    chore: WebClientConfig.java configuration
    Signed-off-by: andrewprasetya-k <andrewprasetya.k@gmail.com>

--- src/main/java/org/warehouse/Config/WebClientConfig.java
+++ src/main/java/org/warehouse/Config/WebClientConfig.java
@@ -2,13 +2,17 @@ package org.warehouse.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder.build();
+    public WebClient webClient(
+            WebClient.Builder builder,
+            @Value("${notification.service.base-url:http://localhost:8081}") String baseUrl
+    ) {
+        return builder.baseUrl(baseUrl).build();
     }
 }
