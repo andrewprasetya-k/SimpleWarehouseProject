2079b17a02e1be05690482d3533a8aa56a9f4ed7
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-09-15 13:52:32 +0700

    feat: change sout to log.info
    Signed-off-by: andrewprasetya-k <andrewprasetya.k@gmail.com>

--- src/main/java/org/warehouse/Service/NotificationService.java
+++ src/main/java/org/warehouse/Service/NotificationService.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.warehouse.Kafka.Dto.ItemsMovedKafkaMessage;
 import reactor.core.publisher.Mono;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class NotificationService {
+    private static final Logger log = LoggerFactory.getLogger(NotificationService.class);
     private final WebClient webClient;
 
     public NotificationService(WebClient webClient) {
@@ -14,18 +17,16 @@ public class NotificationService {
     }
 
     public void notifyItemsMoved(ItemsMovedKafkaMessage message) {
-        System.out.println(
-                "NOTIFICATION_ITEMS_MOVED warehouseId=" + message.warehouseId()
-                        + " itemIds=" + message.itemIds()
-                        + " status=" + message.status()
-        );
+        log.info("NOTIFICATION_ITEMS_MOVED eventId={} sourceWarehouseId={} targetWarehouseId={} itemIds={} status={}",
+                message.eventId(), message.sourceWarehouseId(), message.warehouseId(), message.itemIds(), message.status());
     }
 
     public Mono<Boolean> checkHealth() {
         return webClient.get()
-                .uri("http://localhost:8080/health")
+                .uri("/health")
                 .retrieve()
                 .toBodilessEntity()
-                .map(response -> response.getStatusCode().is2xxSuccessful());
+                .map(response -> response.getStatusCode().is2xxSuccessful())
+                .onErrorReturn(false);
     }
 }
