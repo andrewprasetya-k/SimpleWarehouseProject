d0a03c667ef9f30a28742a457d984cd97c8d2a98
andrewprasetya-k <andrewprasetya.k@gmail.com>
2026-09-21 11:41:01 +0700

    feat: added warehouse caching
    Signed-off-by: andrewprasetya-k <andrewprasetya.k@gmail.com>

--- src/main/java/org/warehouse/Service/WarehouseService.java
+++ src/main/java/org/warehouse/Service/WarehouseService.java
@@ -1,8 +1,12 @@
 package org.warehouse.Service;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.warehouse.Dto.WarehousePagedResponse;
 import org.warehouse.Dto.WarehouseResponse;
 import org.warehouse.Model.WarehouseModel;
@@ -19,6 +23,7 @@ public class WarehouseService {
         this.repo = repo;
     }
 
+    @Cacheable(value="warehouse-pages", key="#pageable.pageNumber + '-' + #pageable.pageSize + '-' + #pageable.sort.toString()")
     public WarehousePagedResponse findAll(Pageable pageable) {
         Page<WarehouseModel> paged = repo.findAll(pageable);
         List<WarehouseResponse> content = new ArrayList<>();
@@ -28,14 +33,27 @@ public class WarehouseService {
         return new WarehousePagedResponse(content, paged.getNumber(),paged.getSize(),paged.getTotalElements(),paged.getTotalPages());
     }
 
+    @Cacheable(value="warehouses", key="#id")
     public WarehouseModel findById(Integer id) {
         return repo.findById(id).orElse(null);
     }
 
+    @Transactional
+    @Caching(evict = {
+            @CacheEvict(value = "warehouses", allEntries = true),
+            @CacheEvict(value = "warehouse-pages", allEntries = true),
+            @CacheEvict(value = "items", allEntries = true)
+    })
     public WarehouseModel save(WarehouseModel warehouse) {
         return repo.save(warehouse);
     }
 
+    @Transactional
+    @Caching(evict = {
+            @CacheEvict(value = "warehouses", allEntries = true),
+            @CacheEvict(value = "warehouse-pages", allEntries = true),
+            @CacheEvict(value = "items", allEntries = true)
+    })
     public WarehouseModel update(Integer id, WarehouseModel warehouse) {
         if (!repo.existsById(id)) {
             return null;
@@ -44,6 +62,12 @@ public class WarehouseService {
         return repo.save(warehouse);
     }
 
+    @Transactional
+    @Caching(evict = {
+            @CacheEvict(value = "warehouses", allEntries = true),
+            @CacheEvict(value = "warehouse-pages", allEntries = true),
+            @CacheEvict(value = "items", allEntries = true)
+    })
     public boolean delete(Integer id) {
         if (repo.existsById(id)) {
             repo.deleteById(id);
